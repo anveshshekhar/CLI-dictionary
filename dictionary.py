f595a1d3f40c198e6ad236d79b7fa9c175018486
anveshshekhar <72605577+anveshshekhar@users.noreply.github.com>
2021-04-26 19:19:23 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -8,16 +8,15 @@ def translate(w):
     if w in data:
         return data[w]
     elif len(get_close_matches(w, data.keys())) > 0:
-        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
-        yn.lower()
-	if yn == "y":
-            return data[get_close_matches(w, data.keys())[0]]
-        elif yn == "n":
-            return "The word doesn't exist. Please double check it."
-        else:
-            return "We didn't understand your entry."
-    else:
-        return "The word doesn't exist. Please double check it."
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0]);
+    
+        if (yn =="y"):
+            return data[get_close_matches(w, data.keys())[0]];
+        elif (yn =="n"):
+         print("Did you mean %s instead? If not, we didn't get your entry: " % get_close_matches(w, data.keys())[1])
+            
+        
+        
 
 word = input("Enter word: ")
 output = translate(word)
